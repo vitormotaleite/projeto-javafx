5b44176e3c255821e2b1e2985ac12289f036dbad
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2022-08-29 17:05:48 -0300

    about view

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,6 +11,8 @@ import javafx.scene.control.ScrollPane;
 
 public class Program extends Application {
 	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) { 
 		try { 
@@ -20,14 +22,18 @@ public class Program extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane); 
+			mainScene = new Scene(scrollPane); 
 			primaryStage.setScene(mainScene); 
 			primaryStage.setTitle("Sample JavaFX application"); 
 			primaryStage.show(); 
 		} 	catch (IOException e) { 
 			e.printStackTrace(); 
 		} 
-	} 
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
+	}
 
 	public static void main(String[] args) {
 		launch(args);
